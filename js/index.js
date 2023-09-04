13eb5f88f5ab925e9545fad97191c6cfe56983b9
VictorM <neuron96@mail.ru>
2023-09-04 07:42:36 +0500

    feat: add js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-console.log('kekw')
+const map = document.querySelector('.map');
+const circles = document.querySelectorAll('.circle');
+
+map.addEventListener('click', (e) => {
+    console.log(e.target.classList.contains('circle'))
+    console.log(circles)
+    if(e.target.classList.contains('circle')) {
+        circles.forEach((item => {
+            removeClosedClass(item)
+        }))
+        if(e.target.closest('.circle')) {
+            e.target.firstChild.textContent = '-'
+            e.target.classList.remove('closed')
+            e.target.classList.add('open')
+        }
+       
+    } else {
+        circles.forEach((item => {
+            removeClosedClass(item)
+        }))
+    }
+    
+})
+
+
+function removeClosedClass(item) {
+    item.classList.remove('open')
+    item.classList.add('closed')
+    item.firstChild.textContent = '+'
+
+}
+
+console.log('map', map);
